1c494f164fdc04c9d775a54c53a69c32fbddb394
Lucas Pereira <lucas4162007@gmail.com>
2025-04-11 08:35:32 -0300

    idk

--- app/app.py
+++ app/app.py
@@ -1,7 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/about_us")
+def about_us():
+    return render_template("aboutus.html")
+
+@app.route("/contacts")
+def contact():
+    return render_template("contact.html")
